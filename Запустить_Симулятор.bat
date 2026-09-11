657b5b265861a5b1fb2f1718a9b633a94f92b76e
HEGO <hego.leg11@gmail.com>
2026-09-11 21:25:39 +0500

    chore: add one-click launch batch script

---
+++ Запустить_Симулятор.bat
@@ -0,0 +1,25 @@
+﻿@echo off
+chcp 65001 >nul
+title Robot Race Simulator
+echo ========================================================
+echo   Запуск Robot Race Simulator
+echo ========================================================
+
+cd /d "%~dp0"
+
+if exist "release\win-unpacked\Line Robot Simulator.exe" (
+    echo Запуск установленного симулятора...
+    start "" "release\win-unpacked\Line Robot Simulator.exe"
+    exit /b 0
+)
+
+if exist "release\Line Robot Simulator Setup 1.1.0.exe" (
+    echo Запуск установщика...
+    start "" "release\Line Robot Simulator Setup 1.1.0.exe"
+    exit /b 0
+)
+
+echo Запуск в веб-режиме разработки...
+start http://localhost:5173
+call npm run dev
+pause
